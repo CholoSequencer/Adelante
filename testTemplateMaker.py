a36ec59860827c348ce3f87a765198738b591ee1
Fernando Vicente Zegarra <fvicentez@FVZ-MacBook.local>
2017-10-19 11:59:37 -0400

    temp 2016iCE for Nico

--- testTemplateMaker.py
+++ testTemplateMaker.py
@@ -1,9 +1,9 @@
-f = open('test_SM2016IIADF.js', 'w')
+f = open('test_SM2016IADF.js', 'w')
 numQ = 130
 f.write("var questions = [\n\t{\n")
 
 for i in range(1,numQ+1):
-	f.write("\t\tname: \"SM2016iiADF" + str(i) + "\",\n" )
+	f.write("\t\tname: \"SM2016iADF" + str(i) + "\",\n" )
 	f.write("\t\tsubject: \"\",\n")
 	f.write("\t\tsub_subject: \"\",\n")
 	f.write("\t\tprompt: false,\n")
